a9bd0f0a4ff66b1799d15297cc9af4ed05d4c52f
Giulio Morandini <giratinamora@gmail.com>
2026-01-07 19:45:25 +0100

    primo test yolov8s

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from ultralytics import YOLO
+
+model = YOLO("yolov8s.pt") #il modello che penso sia meglio usare con il nostro hardware, magari si può upgradare
+
+video_path = "input_videos/video_1.mp4"
+
+results = model.predict(video_path, save=True)
+print(results)
+print("########################")
+for box in results[0].boxes:
+    print(box.xyxy)
