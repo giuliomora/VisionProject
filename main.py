0694e4cfa5d624b0cea44a124cef6c5307dc6ac1
Mrv22 <marco.venanzi01@gmail.com>
2026-01-09 18:23:26 +0100

    ok

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from ultralytics import YOLO
-
+#stocazzoi
 model = YOLO("yolov8s.pt") #il modello che penso sia meglio usare con il nostro hardware, magari si può upgradare
 
 video_path = "input_videos/video_1.mp4"
