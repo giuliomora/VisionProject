ffcf3500aebc33c6d85bbad0bdc2dd9ddf3ca8cb
Mrv22 <marco.venanzi01@gmail.com>
2026-01-14 20:46:29 +0100

    andato avati fino a circa 1:16

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 
 model = YOLO("models/ball_detector_model.pt") #il modello della palla
 video_path = "input_videos/video_1.mp4"
-
-results = model.predict(video_path, save=True)
+#.predict for detection, .track for tracking
+results = model.track(video_path, save=True)
 print(results)
 print("########################")
 for box in results[0].boxes:
---
+++ utils/video_utils.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+
+def read_video(video_path):
+
+    """Reads a video from the given path and returns the video capture object."""
+    
+    cap = cv2.VideoCapture(video_path)
+    frames=[]
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+
+    return frames
+
+def save_video(frames, output_path, ):
+    if not os.path.exists(os.path.dirname(output_path)):
+        os.makedirs(os.path.dirname(output_path))
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out= cv2.VideoWriter(output_path, fourcc, 24.0, (frames[0].shape[1], frames[0].shape[0]))
+
+    for frame in frames:
+        out.write(frame)
+    out.release()
+
