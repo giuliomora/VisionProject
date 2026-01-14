cded86780e3fa87b1f96a29f5d0c85ff15704280
Mrv22 <marco.venanzi01@gmail.com>
2026-01-15 00:08:18 +0100

    funziona

--- utils/main.py
+++ utils/main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+#sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from video_utils import read_video, save_video
 
